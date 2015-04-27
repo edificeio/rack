d1d018ba8e7c9d484aba6bc90b87f4afd9ff5bcd
dboissin <damien.boissin@web-education.net>
2015-04-27 14:41:05 +0200

    [Evo] #3761 : show all groups

--- src/main/java/fr/wseduc/rack/controllers/RackController.java
+++ src/main/java/fr/wseduc/rack/controllers/RackController.java
@@ -428,7 +428,7 @@ public class RackController extends MongoDbControllerHelper {
 
 	private void getVisibleRackUsers(final HttpServerRequest request, final Handler<JsonArray> handler){
 		String customReturn =
-				"MATCH visibles-[:IN]->(g:ProfileGroup) " +
+				"MATCH visibles-[:IN]->(g:Group) " +
 				"MATCH visibles-[:IN]->(:ProfileGroup)-[:AUTHORIZED]->(:Role)-[:AUTHORIZE]->(a:Action) " +
 				"WHERE has(a.name) AND a.name={action} AND NOT has(visibles.activationCode) " +
 				"RETURN distinct visibles.id as id, visibles.displayName as username, visibles.lastName as name, collect(DISTINCT {name: g.name, id: g.id, groupDisplayName: g.groupDisplayName}) as groups " +
