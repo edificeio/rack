cbdea727b82463c3bfabedd671e9701bbf34e09b
Marie Laporte <mmarie.laporte@gmail.com>
2018-09-13 15:13:49 +0200

    Adding watcher for docker

--- build.sh
+++ build.sh
@@ -11,26 +11,72 @@ then
   export GROUP_GID=1000
 fi
 
-if [ "$1" == "clean" ]
-then
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
-fi
-if [ $? -eq 0 ]
-then
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install && npm update entcore && node_modules/gulp/bin/gulp.js build" && docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
-fi
-if [ $? -eq 0 ]
-then
-  if [ "$2" == "publish" ]
-  then
-    if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
-    then
-      echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
-      echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
-      echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
-      echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
-    fi
-    docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
-  fi
-fi
+# options
+SPRINGBOARD="recette"
+for i in "$@"
+do
+case $i in
+    -s=*|--springboard=*)
+    SPRINGBOARD="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
 
+clean () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
+}
+
+buildNode () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install && node_modules/gulp/bin/gulp.js build"
+}
+
+buildGradle () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
+}
+
+watch () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "node_modules/gulp/bin/gulp.js watch --springboard=/home/node/$SPRINGBOARD"
+}
+
+publish () {
+  if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
+  then
+    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
+    echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
+    echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
+    echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
+  fi
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+}
+
+for param in "$@"
+do
+  case $param in
+    clean)
+      clean
+      ;;
+    buildNode)
+      buildNode
+      ;;
+    buildGradle)
+      buildGradle
+      ;;
+    install)
+      buildNode && buildGradle
+      ;;
+    watch)
+      watch
+      ;;
+    publish)
+      publish
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
