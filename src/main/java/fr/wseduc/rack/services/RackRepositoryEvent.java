d13ee8f364b3b09cd6ee8645bb16c7f053ebb30f
nabil <nabil.mansouri@opendigitaleducation.com>
2018-11-30 17:58:23 +0100

    [Fix] change rack export folder root

--- src/main/java/fr/wseduc/rack/services/RackRepositoryEvent.java
+++ src/main/java/fr/wseduc/rack/services/RackRepositoryEvent.java
@@ -111,7 +111,7 @@ public class RackRepositoryEvent implements RepositoryEvents {
 
 	private void createExportDirectory(String exportPath, String locale, final Handler<String> handler) {
 		final String path = exportPath + File.separator
-				+ I18n.getInstance().translate("workspace.title", I18n.DEFAULT_DOMAIN, locale);
+				+ I18n.getInstance().translate("rack.title", I18n.DEFAULT_DOMAIN, locale);
 		vertx.fileSystem().mkdir(path, new Handler<AsyncResult<Void>>() {
 			@Override
 			public void handle(AsyncResult<Void> event) {
